9177a7c26696298b3ab8a971b72dca10aa8b0a79
Rim4i4ok <rim4i4ok@gmail.com>
2024-03-26 22:01:33 +0100

    adding spinnedr for job item request

--- src/lib/hooks/useJobItem.ts
+++ src/lib/hooks/useJobItem.ts
@@ -4,18 +4,23 @@ import { JobItemExpanded } from "../types";
 
 export function useJobItem(id: number | null) {
   const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!id) return;
 
     const fetchData = async () => {
+      setIsLoading(true);
+
       const response = await fetch(`${API_URL}/${id}`);
       const data = await response.json();
+
+      setIsLoading(false);
       setJobItem(data.jobItem);
     };
 
     fetchData();
   }, [id]);
 
-  return jobItem;
+  return [jobItem, isLoading] as const;
 }
