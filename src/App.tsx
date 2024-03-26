633bdc69274586cdfdc1ea6ef1dcc903a4d12c6a
Rim4i4ok <rim4i4ok@gmail.com>
2024-03-26 15:50:10 +0100

    fetching data and props drilling to joblist

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import { useEffect, useState } from "react";
 import Background from "./components/Background";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { API_URL } from "./lib/constants";
 
 function App() {
+  const [searchText, setSearchText] = useState("react");
+  const [jobItems, setJobItems] = useState([]);
+
+  useEffect(() => {
+    if (!searchText) return;
+
+    const fetchData = async () => {
+      const response = await fetch(API_URL + `?search=${searchText}`);
+      const data = await response.json();
+      setJobItems(data.jobItems);
+    };
+
+    fetchData();
+  }, [searchText]);
+
   return (
     <>
       <Background />
 
-      <Header />
+      <Header searchText={searchText} setSearchText={setSearchText} />
 
-      <Container />
+      <Container jobItems={jobItems} />
 
       <Footer />
     </>
