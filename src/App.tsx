67de7f8686708d5dbe7c4ef6a2f901d0fb5a54cb
Rim4i4ok <rim4i4ok@gmail.com>
2024-03-26 14:54:40 +0100

    adding base components

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Background from "./components/Background";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Background />
+
+      <Header />
+
+      <Container />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
