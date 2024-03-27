3f6b2c40453c496a90f402fb4d62eb05710098cb
Rim4i4ok <rim4i4ok@gmail.com>
2024-03-27 16:12:29 +0100

    extracting api call to function

--- src/lib/hooks/useJobItem.ts
+++ src/lib/hooks/useJobItem.ts
@@ -1,15 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { API_URL } from "../constants";
 
+const fetchJobItem = async (id: number) => {
+  const response = await fetch(`${API_URL}/${id}`);
+  const data = await response.json();
+
+  return data;
+};
+
 export function useJobItem(id: number | null) {
   const { data, isLoading } = useQuery(
     ["job-item", id],
-    async () => {
-      const response = await fetch(`${API_URL}/${id}`);
-      const data = await response.json();
-
-      return data;
-    },
+    () => (id ? fetchJobItem(id) : null),
     {
       staleTime: 1000 * 60 * 60,
       refetchOnWindowFocus: false,
