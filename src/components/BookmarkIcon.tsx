bcf62ed746f36854ccf8b3ad90e00769be171d46
Rim4i4ok <rim4i4ok@gmail.com>
2024-03-28 15:40:47 +0100

    stop propagation for icon button

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -9,7 +9,15 @@ export default function BookmarkIcon({ id }: BookmarkIconProps) {
   const { bookmarkedIds, handleToggleBookmark } = useBookmarksContext();
 
   return (
-    <button className="bookmark-btn" onClick={() => handleToggleBookmark(id)}>
+    <button
+      className="bookmark-btn"
+      onClick={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+
+        handleToggleBookmark(id);
+      }}
+    >
       <BookmarkFilledIcon
         className={`${bookmarkedIds.includes(id) && "filled"}`}
       />
