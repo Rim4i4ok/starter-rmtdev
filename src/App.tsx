45d341984c0db219c4ebf2785b5a09f7b8f5ca1a
Rim4i4ok <rim4i4ok@gmail.com>
2024-03-28 13:35:07 +0100

    extracting constant for items per page

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import SortingControls from "./components/SortingControls";
 import { useDebounce, useJobItems } from "./lib/hooks";
 import { Toaster } from "react-hot-toast";
 import { Direction } from "./lib/types";
+import { RESULTS_PER_PAGE } from "./lib/constants";
 
 function App() {
   // state
@@ -25,8 +26,11 @@ function App() {
 
   // derived / computed state
   const totalNumberOfResults = jobItems.length;
-  const totalNumberOfPages = Math.ceil(totalNumberOfResults / 7);
-  const jobItemsSliced = jobItems.slice((currentPage - 1) * 7, currentPage * 7);
+  const totalNumberOfPages = Math.ceil(totalNumberOfResults / RESULTS_PER_PAGE);
+  const jobItemsSliced = jobItems.slice(
+    (currentPage - 1) * RESULTS_PER_PAGE,
+    currentPage * RESULTS_PER_PAGE
+  );
 
   // event handlers / actions
   const handleChangePage = (direction: Direction) => {
